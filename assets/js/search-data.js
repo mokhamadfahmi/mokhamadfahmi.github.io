02bd8ff46b8635a393c2e6422dd0bf5f124fa5f5
mokhamadfahmi <mokhamadfahmi@gmail.com>
2026-04-02 04:22:07 +0000

    Deploying to gh-pages from @ mokhamadfahmi/mokhamadfahmi.github.io@b1166593ba34f38f8d4ecfc1fd244f2c10ef972c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-am-thrilled-to-announce-that-our-paper-structural-basis-underlying-the-autoinhibition-of-the-formin-fhod1-and-its-phosphorylation-dependent-activation-has-been-published-in-the-journal-of-biological-chemistry-jbc",
+          title: 'I am thrilled to announce that our paper, “Structural basis underlying the autoinhibition...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
